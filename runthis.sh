4ee7d835b29a0a20d33be17172c983efcca2275b
Parth <parthbangde123@gmail.com>
2024-04-24 16:36:27 +0530

    added name to bash script

--- runthis.sh
+++ runthis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python main.py --optimizer SGD --online-mode Binary --weights Normal --max-epoch 50 
-python main.py --optimizer Adam --online-mode Multi --weights Normal --max-epoch 50 
-python main.py --optimizer SGD --online-mode Multi --weights Normal --max-epoch 50 
-python main.py --optimizer Adam --online-mode Binary --weights Inverse --max-epoch 50 
-python main.py --optimizer Adam --online-mode Multi --weights Inverse --max-epoch 50 
-python main.py --optimizer SGD --online-mode Multi --weights Inverse --max-epoch 50 
-python main.py --optimizer SGD --online-mode Binary --weights Inverse --max-epoch 50 
+python main.py --optimizer SGD --online-mode Binary --weights Normal --max-epoch 50 --model-name wsanodet_SGD_Binary_Normal_50
+python main.py --optimizer Adam --online-mode Multi --weights Normal --max-epoch 50 --model-name wsanodet_Adam_Multi_Normal_50 
+python main.py --optimizer SGD --online-mode Multi --weights Normal --max-epoch 50 --model-name wsanodet_SGD_Multi_Normal_50 
+python main.py --optimizer Adam --online-mode Binary --weights Inverse --max-epoch 50 --model-name wsanodet_Adam_Binary_Inverse_50 
+python main.py --optimizer Adam --online-mode Multi --weights Inverse --max-epoch 50 --model-name wsanodet_Adam_Multi_Inverse_50 
+python main.py --optimizer SGD --online-mode Multi --weights Inverse --max-epoch 50 --model-name wsanodet_SGD_Multi_Inverse_50 
+python main.py --optimizer SGD --online-mode Binary --weights Inverse --max-epoch 50 --model-name wsanodet_SGD_Binary_Inverse_50 
