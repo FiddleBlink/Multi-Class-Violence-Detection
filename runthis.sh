e78ea82188322bfeee869a64463dd754c6fded83
Parth <parthbangde123@gmail.com>
2024-04-26 13:43:56 +0530

    updated the bash script

--- runthis.sh
+++ runthis.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-python main.py --optimizer SGD --online-mode Binary --weights Normal --max-epoch 50 --model-name wsanodet_SGD_Binary_Normal_50
-python main.py --optimizer Adam --online-mode Multi --weights Normal --max-epoch 50 --model-name wsanodet_Adam_Multi_Normal_50 
-python main.py --optimizer SGD --online-mode Multi --weights Normal --max-epoch 50 --model-name wsanodet_SGD_Multi_Normal_50 
-python main.py --optimizer Adam --online-mode Binary --weights Inverse --max-epoch 50 --model-name wsanodet_Adam_Binary_Inverse_50 
-python main.py --optimizer Adam --online-mode Multi --weights Inverse --max-epoch 50 --model-name wsanodet_Adam_Multi_Inverse_50 
-python main.py --optimizer SGD --online-mode Multi --weights Inverse --max-epoch 50 --model-name wsanodet_SGD_Multi_Inverse_50 
-python main.py --optimizer SGD --online-mode Binary --weights Inverse --max-epoch 50 --model-name wsanodet_SGD_Binary_Inverse_50 
+python main.py --optimizer Adam --online-mode Binary --weights Normal --max-epoch 100 --model-name wsanodet_SGD_Binary_Normal_100
+python main.py --optimizer Adam --online-mode Binary --weights Normal --max-epoch 150 --model-name wsanodet_SGD_Binary_Normal_150
+python main.py --optimizer Adam --online-mode Binary --weights Inverse --max-epoch 100 --model-name wsanodet_Adam_Binary_Inverse_100 
+python main.py --optimizer Adam --online-mode Binary --weights Inverse --max-epoch 150 --model-name wsanodet_Adam_Binary_Inverse_150 
+python main.py --optimizer Adam --online-mode Multi --weights Normal --max-epoch 100 --model-name wsanodet_Adam_Multi_Normal_100 
+python main.py --optimizer Adam --online-mode Multi --weights Normal --max-epoch 150 --model-name wsanodet_Adam_Multi_Normal_150 
+python main.py --optimizer SGD --online-mode Multi --weights Normal --max-epoch 100 --model-name wsanodet_SGD_Multi_Normal_100 
+python main.py --optimizer SGD --online-mode Multi --weights Normal --max-epoch 150 --model-name wsanodet_SGD_Multi_Normal_150 
+python main.py --optimizer SGD --online-mode Binary --weights Normal --max-epoch 100 --model-name wsanodet_SGD_Binary_Normal_100
+python main.py --optimizer SGD --online-mode Binary --weights Normal --max-epoch 150 --model-name wsanodet_SGD_Binary_Normal_150
